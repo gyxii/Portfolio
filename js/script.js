6ed119da895dde4bc942565574db325857a12cfa
Song's <100990317+gyxii@users.noreply.github.com>
2022-03-16 00:02:41 +0900

    JS:navbar_toggle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-// transparent => color navBar top
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -11,41 +11,38 @@ document.addEventListener('scroll', () => {
   }
 });
 
-//navbar menu scrolling 
-
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-
 navbarMenu.addEventListener('click', (event) => {
-
-const target = event.target;
-const link = target.dataset.link;
-if (link == null) {
-  return;
-}
-navbarMenu.classList.remove('open');
-
-console.log(event.target.dataset.link);
-const scrollTo = document.querySelector(link);
-scrollTo.scrollIntoView({behavior:'smooth'});
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
 });
 
-//contact me button
-const homeConractBtn = document.querySelector('.home__contact');
-homeConractBtn.addEventListener('click' , () => {
-  const scrollTo = document.querySelector('#contact');
-  scrollTo.scrollIntoView({behavior:'smooth'});
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
 
-//transparent scroll
+// Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-
-//show arrow-up button scrolling up down
+// Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
   if (window.scrollY > homeHeight / 2) {
@@ -55,9 +52,9 @@ document.addEventListener('scroll', () => {
   }
 });
 
-//Handle click "arrow up"
-arrowUp.addEventListener('click' , ()=>{
-scrollIntoView('#home');
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
 });
 
 // Projects
@@ -95,5 +92,3 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
-
-
