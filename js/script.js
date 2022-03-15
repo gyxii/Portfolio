308b0ec767ed22cc6165187530d9243b5d6df780
Song's <100990317+gyxii@users.noreply.github.com>
2022-03-15 20:44:10 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,23 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+//navbar menu scrolling 
+
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event) => {
+
+const target = event.target;
+const link = target.dataset.link;
+if (link == null) {
+  return;
+}
+navbarMenu.classList.remove('open');
+
+console.log(event.target.dataset.link);
+
+const scrollTo = document.querySelector(link);
+scrollTo.scrollIntoView();
+});
+
